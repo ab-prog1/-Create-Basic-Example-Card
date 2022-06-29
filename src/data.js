5e9356cb19c0c126d0e8c4f568475c9f59a6d203
Abolfazl Ghorbani <ab.ghorbani28@gmail.com>
2022-06-29 15:39:12 +0200

    Create Basic Example Card

---
+++ src/data.js
@@ -0,0 +1,29 @@
+const reviews = [
+    {
+        id: 1,
+        name: 'Nima Moha',
+        job: ' Web designer',
+        image: 'https://images.pexels.com/photos/9154716/pexels-photo-9154716.jpeg?cs=srgb&dl=pexels-cottonbro-9154716.jpg&fm=jpg',
+        text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+    
+    },
+    {
+        id: 2,
+        name: 'Ali Mohajer ',
+        job: 'Graphic Designer',
+        image: 'https://images.pexels.com/photos/428364/pexels-photo-428364.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
+        text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+    
+    },
+    {
+        id: 3,
+        name: 'Sara Mohajer',
+        job: 'programmer',
+        image: 'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
+        text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+    
+    }
+]
+
+
+export default reviews;
